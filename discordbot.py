831d65ad5279441714bc12e9f4ba70f18398a516
popanoas <60436785+popanoas@users.noreply.github.com>
2020-11-10 02:11:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord
-import asyncio
-import os
+intents=discord.Intents.all()
+intents.typing = False
 import traceback
 import random
 from datetime import datetime
@@ -12,7 +12,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 
 # 接続に必要なオブジェクトを生成
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 ID_CHANNEL_README = 768272323341320232 # 該当のチャンネルのID
 ID_ROLE_WELCOME = 666361330827132979 # 当月クランメンバーのID
