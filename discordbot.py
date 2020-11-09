13b447c5b700dce48fe080e7c1773e024d662466
popanoas <60436785+popanoas@users.noreply.github.com>
2020-11-10 01:34:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,6 +14,9 @@ token = os.environ['DISCORD_BOT_TOKEN']
 ID_readme = 768272323341320232
 ID_clanmember = 666361330827132979
 
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
 @client.event  
 async def on_raw_reaction_add(payload):  
     channel = client.get_channel(payload.channel_id)
