d5e3c00ae92a67d7a2b9a686e8902308f64f4fd9
popanoas <60436785+popanoas@users.noreply.github.com>
2020-12-30 10:43:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client(intents=intents)
 
 ID_CHANNEL_README = 768272323341320232 #readmeチャンネルのID
-ID_ROLE_WELCOME = 666361330827132979 # 当月クランメンバーのID
+ID_ROLE_WELCOME = 666361330827132979 # わるいねのID
 
 @client.event
 async def on_raw_reaction_add(payload):
@@ -33,5 +33,18 @@ async def on_raw_reaction_add(payload):
     msg = await channel.send('ようこそ！' + member.name + 'さん！')
     await asyncio.sleep(5) 
     await msg.delete()
+
+@tasks.loop(seconds=60)
+async def totuDeclaration():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '05:00':
+  
+        #ランドソル杯データ入力
+        #channel = client.get_channel(ID_Mana)
+        #msg = await channel.send('日付が変わりました！記入が終わったらリアクションを付けてね♡ \n https://docs.google.com/spreadsheets/d/1nCdtFHS-60WcRZDx8hTXHFm3mPuEqefntQxeRfM2Lv0/edit#gid=632518118')  
+        #await msg.add_reaction(ID_emoji)
+        
+loop.start()          
     
 client.run(token)
